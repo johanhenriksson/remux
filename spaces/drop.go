1ae3ad23298d7f896f4d8ffc6a96eb6389b14289
Johan Henriksson <johan.henriksson@eqtpartners.com>
2026-02-25 17:01:22 +0100

    fix: kill tmux session before drop hooks

--- spaces/drop.go
+++ spaces/drop.go
@@ -3,7 +3,9 @@ package spaces
 import (
 	"fmt"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"syscall"
 
 	"github.com/johanhenriksson/remux/git"
 	"github.com/johanhenriksson/remux/registry"
@@ -26,9 +28,17 @@ func Drop(worktreePath string, force bool) error {
 		return fmt.Errorf("failed to find main repository: %w", err)
 	}
 
+	spaceName := filepath.Base(worktreePath)
+
+	// Ignore SIGHUP so we survive if we're running inside the tmux session being killed
+	signal.Ignore(syscall.SIGHUP)
+
+	// Kill the tmux session first so that processes (e.g. servers) started by tabs
+	// are stopped before on_drop hooks run (e.g. dropping a database)
+	tmux.KillSession(spaceName)
+
 	// Run on_drop hooks before removal (abort on failure)
 	// If space isn't registered, skip hooks but continue with removal
-	spaceName := filepath.Base(worktreePath)
 	if space, err := Open(worktreePath); err == nil {
 		if err := space.RunOnDrop(); err != nil {
 			return err
@@ -50,8 +60,6 @@ func Drop(worktreePath string, force bool) error {
 		reg.Remove(spaceName)
 		_ = reg.Save(destDir)
 	}
-
-	tmux.KillSession(spaceName)
 
 	return nil
 }
